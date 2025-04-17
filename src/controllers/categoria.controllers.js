08bb8ffad1abc1344989df349226ad4f06a99333
YeisonSegura <yeisonrinconcr7@gmail.com>
2025-04-17 00:01:32 -0500

    Eliminar Registros - Metodo HTTP - DELETE

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -28,7 +28,7 @@ const postCategorias = async (req, res) => {
 
 const getCategory = async (req, res)=>{
     try {
-        console.log(req.param);
+        console.log(req.params);
         const {id} = req.params
         const connection = await getConnection();
         const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias WHERE CategoriaID = ?",id)
@@ -38,8 +38,21 @@ const getCategory = async (req, res)=>{
     }
 }
 
+const deleteCategory = async (req, res)=>{
+    try {
+        console.log("id de categoria a borrar: ", req.params);
+        const {id} = req.params
+        const connection = await getConnection();
+        const result = await connection.query("DELETE FROM categorias WHERE CategoriaID = ?",id)
+        res.json(result);
+    } catch (error) {
+        console.error("ERROR 500");
+    }
+}
+
 export const methodHTTP = {
     getCategorias,
     postCategorias,
-    getCategory
+    getCategory,
+    deleteCategory
 }
--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -5,11 +5,14 @@ import { methodHTTP as categoriaController } from "../controllers/categoria.cont
 const router = Router();
 
 // configuramos respuesta desde aerver metodo http get
-router.get("/", categoriaController.getCategorias); //read
-router.post("/", categoriaController.postCategorias); //create
+router.get("/", categoriaController.getCategorias); //para crud - read
+router.post("/", categoriaController.postCategorias); //para crud - create
 
 // Ruta que recibe un parametro
-router.get("/:id", categoriaController.getCategory); //read
+router.get("/:id", categoriaController.getCategory); //para crud - read 1 sola fila
+
+// Ruta que recibe parametro id de categoria a borrar
+router.delete("/:id", categoriaController.deleteCategory); //para delete - read 1 sola fila
 
 // hacemos disponible a router en toda la app
 export default router;
